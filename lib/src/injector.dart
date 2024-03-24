8b6776c897c229aea74f81ce559a2d7c65a99786
rcshpark <rcshpark@gmail.com>
2024-03-24 14:59:53 +0900

    feat : Add nutrient injector

--- lib/src/injector.dart
+++ lib/src/injector.dart
@@ -1,14 +1,26 @@
 import 'package:get_it/get_it.dart';
 import 'package:health_care/src/data/data_sources/auth_data_source.dart';
+import 'package:health_care/src/data/data_sources/nutrient_data_source.dart';
 import 'package:health_care/src/data/repository/auth_repository_impl.dart';
+import 'package:health_care/src/data/repository/nutrient_repository_impl.dart';
 import 'package:health_care/src/domain/repository/auth_repository.dart';
+import 'package:health_care/src/domain/repository/nutrient_repository.dart';
 import 'package:health_care/src/domain/usecase/auth_usecase.dart';
+import 'package:health_care/src/domain/usecase/nutrient_usecase.dart';
 
 final getIt = GetIt.instance;
 
 void setupInjector() {
+  // data source
   getIt.registerLazySingleton<AuthDataSource>(() => AuthDataSource());
+  getIt.registerLazySingleton<NutrientDataSource>(() => NutrientDataSource());
+
+  // repository
   getIt
       .registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(getIt()));
+  getIt.registerLazySingleton<NutrientRepository>(
+      () => NutrientRepositoryImpl(getIt()));
+
   getIt.registerLazySingleton<AuthUseCase>(() => AuthUseCase(getIt()));
+  getIt.registerLazySingleton<NutrientUseCase>(() => NutrientUseCase(getIt()));
 }

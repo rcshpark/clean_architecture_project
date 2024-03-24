81b17ff60039777fb7fa5844ca6b61855034ca6f
rcshpark <rcshpark@gmail.com>
2024-03-24 20:29:14 +0900

    feat : Add auth params(login type)

--- lib/src/domain/usecase/auth_usecase.dart
+++ lib/src/domain/usecase/auth_usecase.dart
@@ -1,15 +1,23 @@
+import 'package:health_care/src/core/enum.dart';
 import 'package:health_care/src/core/usecase/usecase.dart';
 import 'package:health_care/src/domain/model/data_state_model.dart';
 import 'package:health_care/src/domain/model/user_model.dart';
 import 'package:health_care/src/domain/repository/auth_repository.dart';
 
-class AuthUseCase implements UseCase<DataState<UserModel>, NoParams> {
+class AuthUseCase implements UseCase<DataState<UserModel>, AuthParam> {
   final AuthRepository _remoteRepository;
 
   AuthUseCase(this._remoteRepository);
 
   @override
-  Future<DataState<UserModel>> call(NoParams) {
-    return _remoteRepository.loginWithKakao();
+  Future<DataState<UserModel>> call(AuthParam authParam) {
+    switch (authParam.loginType) {
+      case LoginType.kakao:
+        return _remoteRepository.loginWithKakao();
+      case LoginType.apple:
+        return _remoteRepository.loginWithApple();
+      default:
+        throw UnimplementedError("Login type not supported");
+    }
   }
 }
